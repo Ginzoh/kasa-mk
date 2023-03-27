d024fbf40f926b0140af8636ebc5782741bd2cfb
KRAUTH Mickael <mickaell.kr@gmail.com>
2023-03-27 23:28:10 +0200

    mobile layout done

--- src/views/ErrorPage/ErrorPage.js
+++ src/views/ErrorPage/ErrorPage.js
@@ -1,11 +1,27 @@
 import './ErrorPage.css';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const ErrorPage = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <main className='ErrorContent'>
       <h1 className='Error404'>404</h1>
-      <p className='oups'> Oups! La page que vous demandez n'existe pas</p>
+      <p className='oups'> Oups! La page que {isMobile ? <br /> : ''} vous demandez n'existe pas</p>
       <Link to='/'><p className='homeLink'>Retourner sur la page d'acceuil</p></Link>
     </main>
   );
